647ab32d4e055c9f2158e5effd6775529e32ead7
Andrew Clawson <andrewclawson@Andrews-MacBook-Pro.local>
2016-07-29 20:24:06 -0600

    Registration form deploys.

--- CONSOLE/views.py
+++ CONSOLE/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 # Create your views here.
 
 def RegistrationForm(request):
-    return HttpResponse("Make a request here")
+    template = loader.get_template('Registration/RegistrationForm.html')
+    return HttpResponse(template.render(request))
